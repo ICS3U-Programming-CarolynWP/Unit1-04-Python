4681dc9cb069f2988551e392f3ff232291b2fa5a
Carolyn Webster Pless <carolyn.webster-pless@stu.ocsb.ca>
2022-09-17 01:19:07 +0000

    Did math calculations

---
+++ area_per_rect.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created by: Carolyn Webster Pless
+# Created on: 22/09/16
+# Calculates area and perimeter of a rectangle with a length of 20m and a width of 34m
+
+
+def main():
+    print ("For a rectangle with dimensions:")
+    print ("20m length, 34m width.")
+    print ("The area is = {} cm^2". format (20*34))
+    print ("The perimeter is = {} cm". format (2*(20+34)))
+
+if __name__ == "__main__":
+  main()
+  
---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created by: Carolyn Webster Pless
+# Created on: 22/09/16
+# Does some math calculations
+
+# To be able to use the square root function
+import math 
+
+def main():
+	print ("Some math for you:")
+	print ("16 + 7 = {}".format(16+7))
+	print ("22 - 9 = {}".format(22-9))
+	print ("37 * 2 = {}".format(37*2))
+	print ("94 / 5 = {}".format(94/5))
+	print ("4^7 = {}".format(4**7))
+	print ("√36 = {}".format(math.sqrt(36)))
+
+if __name__ == "__main__":
+  main()
